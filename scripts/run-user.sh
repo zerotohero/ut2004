eef895d60b79350b3351d1561480090aa171ab0b
Greg <reflectivecode@users.noreply.github.com>
2017-01-19 21:01:59 -0600

    initial commit

---
+++ scripts/run-user.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+download.sh
+
+config.sh
+
+compress.sh
+
+echo "Setting permissions on ${UT2004_HOME}/.ut2004"
+mkdir -p "${UT2004_HOME}/.ut2004"
+chown -R ut2004:ut2004 "${UT2004_HOME}/.ut2004"
+chmod -R a=,u=rwX "${UT2004_HOME}/.ut2004"
+
+echo "Starting unreal server"
+exec "${UT2004_UCC}" server "CTF-FACECLASSIC?game=XGame.xCTFGame"
